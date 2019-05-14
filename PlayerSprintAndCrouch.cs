dcb686688d005168e54eecdae31694b8d95d265b
Laura Cernat <47827821+lauracernat21@users.noreply.github.com>
2019-05-14 22:08:41 +0300

    Add files via upload
    Am creat functii pentru miscarea caracterului principal, cat si pentru optiunile cursorului.

---
+++ PlayerSprintAndCrouch.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    public float sprint_Speed = 10f;
+    public float move_Speed = 5f;
+    public float crouch_Speed = 2f;
+
+    private Transform look_Root;
+    private float stand_Height = 1.6f;
+    private float crouch_Height = 1f;
+
+    private bool is_Crouching;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+
+        look_Root = transform.GetChild(0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sprint();
+        Crouch();
+    }
+
+    void Sprint()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching)
+        {
+            playerMovement.speed = sprint_Speed;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching)
+        {
+            playerMovement.speed = move_Speed;
+        }
+    }
+
+    void Crouch()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            // if the character is crouching - stand up
+            if(is_Crouching)
+            {
+                look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
+                playerMovement.speed = move_Speed;
+
+                is_Crouching = false;
+            }
+            else
+            {
+                //if the character is not crouching - crouch
+                look_Root.localPosition = new Vector3(0f, crouch_Height, 0f);
+                playerMovement.speed = crouch_Speed;
+
+                is_Crouching = true;
+
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
